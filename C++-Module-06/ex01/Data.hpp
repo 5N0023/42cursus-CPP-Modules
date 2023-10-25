cd3b90f8e4c6f4fc80e4eff43af7072c0d0017c3
5N0023 <lektaibim@gmail.com>
2023-10-25 12:15:21 +0100

    done

---
+++ C++-Module-06/ex01/Data.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#include <iostream>
+
+struct Data
+{
+    char c1;
+    int n;
+    char c2;
+};
---
+++ C++-Module-06/ex01/main.cpp
@@ -0,0 +1,21 @@
+#include "Serializer.hpp"
+
+int main()
+{
+    Data *data = new Data;
+    data->c1 = 'A';
+    data->n = 1337;
+    data->c2 = 'B';
+
+    uintptr_t raw = Serializer::serialize(data);
+    Data *data2 = Serializer::deserialize(raw);
+
+    std::cout << "data->c1: " << data->c1 << std::endl;
+    std::cout << "data->n: " << data->n << std::endl;
+    std::cout << "data->c2: " << data->c2 << std::endl;
+    std::cout << "data2->c1: " << data2->c1 << std::endl;
+    std::cout << "data2->n: " << data2->n << std::endl;
+    std::cout << "data2->c2: " << data2->c2 << std::endl;
+    delete data;
+    return 0;
+}
